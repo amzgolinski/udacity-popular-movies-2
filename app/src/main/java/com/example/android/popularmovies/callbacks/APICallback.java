81103712803ad36398f2b97ed984cb459ed2fc58
Alex Zgolinski <azgolinski@azgolinski-macbookpro2.roam.corp.google.com>
2016-01-10 17:47:21 -0800

    Adding application files for Popular Movies Stage 2.

---
+++ app/src/main/java/com/example/android/popularmovies/callbacks/APICallback.java
@@ -0,0 +1,7 @@
+package com.example.android.popularmovies.callbacks;
+
+public interface APICallback {
+
+  public void onCallCompleted();
+
+}
---
+++ app/src/main/java/com/example/android/popularmovies/callbacks/ItemSelectedCallback.java
@@ -0,0 +1,10 @@
+package com.example.android.popularmovies.callbacks;
+
+import android.net.Uri;
+
+public interface ItemSelectedCallback {
+
+  public void onItemSelected(Uri dateUri);
+
+}
+
